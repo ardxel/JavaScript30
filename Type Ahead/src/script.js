30507b146a8218feee3e903f3cea991175eccfdf
ardxel <ardxel@gmail.com>
2022-12-26 23:26:04 +0300

    compele

--- Type Ahead/src/script.js
+++ Type Ahead/src/script.js
@@ -8,29 +8,75 @@ async function fetchingCities() {
         .then(data => cities.push(...data))
 }
 
-fetchingCities();
+fetchingCities()
 const cities = [];
 
 console.log(cities);
 
 function filterByInput(input) {
-    const output = cities.filter(item => {
-        let city = item.city.toLowerCase();
-        return city.includes(String(input));
-    });
-    return output;
+    return cities.map(value => {
+            return {
+                state: value.state,
+                city: value.city,
+                population: value.population
+            }
+        }
+    )
+        .filter(item => {
+            const city = item.city.toLowerCase();
+            const state = item.state.toLowerCase();
+            const output = city + ' ' + state;
+            return output.includes(input);
+        });
 }
+
+function formatNumberOfPopulation(number) {
+    return new Intl.NumberFormat('en-US').format(number);
+}
+
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+});
 
 function matchCities() {
     list.innerHTML = '';
     const cities = filterByInput(this.value);
-
+    const regexp = new RegExp(this.value, 'gi');
     for (let value of cities) {
-        let li = document.createElement('li');
-        li.innerHTML = value.city;
+        const li = document.createElement('li');
+        const spanMain = document.createElement('span');
+        const spanPop = document.createElement('span');
+
+        let city = value.city;
+        let state = value.state;
+        // allNames = allNames.replace(regexp, `<span class="hl">${searchInput.value}</span>`);
+        city = city.replace(regexp, `<span class="hl">${searchInput.value}</span>`);
+        city = city.capitalize();
+
+        state = state.replace(regexp, `<span class="hl">${searchInput.value}</span>`);
+        state = state.capitalize();
+
+        const population = formatNumberOfPopulation(value.population);
+
+        let allNames = `${state} ${city}`;
+
+        spanMain.classList.add('tagged');
+        // spanMain.innerHTML = `${state} ${city}`;
+        spanMain.innerHTML = allNames;
+        spanPop.style.fontSize = 16+'px';
+        spanPop.innerHTML = population;
+        li.append(spanMain, spanPop);
+
+
         list.append(li);
     }
+
 }
 
-searchInput.addEventListener('input', matchCities)
+
+searchInput.addEventListener('input', matchCities);
+// list.addEventListener('change', markTeggedLi)
 
